aee9b0a12abcae352ad24f1e25922ccd0bec3bb7
jiangdequan <jiangdequan@travelsky.com.cn>
2022-02-23 23:59:03 +0800

    删除文件中的hash

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,16 @@ module.exports = {
     devServer: {
         disableHostCheck: true,
         port: 8080,
+    },
+    chainWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            // 给js和css配置版本号
+            config.output.filename('js/[name].js').end();
+            config.output.chunkFilename('js/[name].js').end();
+            config.plugin('extract-css').tap(args => [{
+                filename: `css/[name].css`,
+                chunkFilename: `css/[name].css`
+            }])
+        }
     }
 }
